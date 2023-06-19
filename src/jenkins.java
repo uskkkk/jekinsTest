e83cb4f5c2368402e7a71e722b685c8d9d5aadce
uskkkk <dntjd4562@gmail.com>
2023-06-19 22:48:20 +0900

    init

--- src/jenkins.java
+++ src/jenkins.java
@@ -1,5 +1,10 @@
 public class jenkins {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins!");
+
+        System.out.println("==============");
+
+        System.out.println("After CI/CD");
+
     }
 }
